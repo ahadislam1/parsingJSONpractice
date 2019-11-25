e9bdfaca5ac62008762e903237c53823b22c72c4
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-11-25 12:08:03 -0500

    Initial Commit

---
+++ parsingJSONpractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parsingJSONpractice
+//
+//  Created by Ahad Islam on 11/25/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
